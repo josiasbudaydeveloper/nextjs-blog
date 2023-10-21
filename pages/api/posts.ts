997653104c5d3d7cd9f219e21368b7b83ff7838e
Josias Buday Dias <josiasbudaydeveloper@gmail.com>
2023-10-20 21:04:05 -0300

    Adding TypeScript

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -1,6 +1,7 @@
 import { getSortedPostsData } from '../../utils/posts';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req, res) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const allPostsData = getSortedPostsData();
   res.status(200).json({ allPostsData });
 }
